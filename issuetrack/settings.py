e150d616d7e0317e3eff65cbf6d0ed7aff37ecd4
rafocus <rafikar@protonmail.com>
2019-07-11 22:02:09 +0100

    updated settings

--- issuetrack/settings.py
+++ issuetrack/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sihgpihc3b9zth)1sd#2cxwc9w_-za8_0!8cnhuny%4z5!w9m)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# value stored in environment variable
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', 'https://issuetrack2019.herokuapp.com']
 
