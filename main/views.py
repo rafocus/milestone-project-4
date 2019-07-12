4135aa950f4ea8fa9b8d06387c50b191bb83b45d
rafocus <rafikar@protonmail.com>
2019-07-12 11:16:02 +0100

    basic bootstrap template with dummy data

--- main/views.py
+++ main/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+tickets = [
+    {
+        'id': '1',
+        'author': 'user1',
+        'title': 'ticket 1',
+        'description': 'this is a ticket 1',
+        'date': '12 July 2019',
+    },
+    {
+        'id': '2',
+        'author': 'user2',
+        'title': 'ticket 2',
+        'description': 'this is a ticket 2',
+        'date': '11 July 2019',
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    context = {
+        'tickets': tickets
+    }
+    return render(request, 'main/home.html', context)
 
 def about(request):
     return render(request, 'main/about.html')
