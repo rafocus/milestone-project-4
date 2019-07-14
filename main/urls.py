d892eefc34b6b4c4f02b8a5759bf27da40b28092
rafocus <rafikar@protonmail.com>
2019-07-14 13:15:30 +0100

    ticket list and detail views using generic views

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import (
+    TicketListView,
+    TicketDetailView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='main-home'),
+    path('', TicketListView.as_view(), name='main-home'),
+    path('ticket/<int:pk>/', TicketDetailView.as_view(), name='ticket-detail'),
     path('about/', views.about, name='main-about'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .models import Ticket
+from django.views.generic import ListView, DetailView
 
 
+class TicketListView(ListView): # generic list view, variable passed is object_list
+    model = Ticket
+    ordering = ['-date']
 
-# Create your views here.
-def home(request):
-    context = {
-        'tickets': Ticket.objects.all()
-    }
-    return render(request, 'main/home.html', context)
+class TicketDetailView(DetailView): #generic detail view, variable passed is object
+    model = Ticket
 
 def about(request):
     return render(request, 'main/about.html')
