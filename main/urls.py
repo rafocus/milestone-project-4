e62e16f90cb92013d8f0671c4dd0b7986cb83b12
rafocus <rafikar@protonmail.com>
2019-07-15 11:46:48 +0100

    ticket create, update, delete

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from .views import (
     TicketListView,
-    TicketDetailView
+    TicketDetailView,
+    TicketCreateView,
+    TicketUpdateView,
+    TicketDeleteView
 )
 from . import views
 
 urlpatterns = [
     path('', TicketListView.as_view(), name='main-home'),
     path('ticket/<int:pk>/', TicketDetailView.as_view(), name='ticket-detail'),
+    path('ticket/new/', TicketCreateView.as_view(), name='ticket-create'),
+    path('ticket/<int:pk>/update/', TicketUpdateView.as_view(), name='ticket-update'),
+    path('ticket/<int:pk>/delete/', TicketDeleteView.as_view(), name='ticket-delete'),
     path('about/', views.about, name='main-about'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Ticket
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 class TicketListView(ListView): # generic list view, variable passed is object_list
@@ -9,6 +10,38 @@ class TicketListView(ListView): # generic list view, variable passed is object_l
 
 class TicketDetailView(DetailView): #generic detail view, variable passed is object
     model = Ticket
+
+class TicketCreateView(LoginRequiredMixin, CreateView):
+    model = Ticket
+    fields = ['title', 'description']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class TicketUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Ticket
+    fields = ['title', 'description']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        ticket = self.get_object()
+        if self.request.user == ticket.author:
+            return True
+        return False
+
+class TicketDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Ticket
+    success_url = '/'
+
+    def test_func(self):
+        ticket = self.get_object()
+        if self.request.user == ticket.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'main/about.html')
