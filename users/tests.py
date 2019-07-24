82286289ec76628425783cc8841acd3ef012967d
rafocus <rafikar@protonmail.com>
2019-07-24 19:58:30 +0100

    user registration form test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from . import forms
 
-# Create your tests here.
+class TestUserRegisterForm(TestCase):
+    def test_form_is_valid(self):
+        form = forms.UserRegisterForm({
+            'username' : 'testusername',
+            'email' : 'testemail@email.com',
+            'password1' : 'testpasstestpass',
+            'password2' : 'testpasstestpass'
+            })
+        self.assertTrue(form.is_valid())
