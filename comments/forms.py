7c80f6193a38ba6dc760de13f0d41f3b7f1ea3ca
rafocus <rafikar@protonmail.com>
2019-07-28 19:45:29 +0100

    comment form placeholder

--- comments/forms.py
+++ comments/forms.py
@@ -11,3 +11,7 @@ class CommentForm(forms.ModelForm):
         labels = {
             "content": ""
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['content'].widget.attrs['placeholder'] = 'Add a comment:'
