0e5b4e288aa52d9ae0c4481e6bc39b03bcdeb864
rafocus <rafikar@protonmail.com>
2019-07-28 19:09:42 +0100

    message sent confirmation

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
 from .models import Message
+from django.contrib import messages
 
 class MessageCreateView(LoginRequiredMixin, CreateView):
     model = Message
@@ -9,4 +10,5 @@ class MessageCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        messages.success(self.request, f'Thank you {self.request.user.username} for contacting us, we will get back to you as soon as possible!')
         return super().form_valid(form)
